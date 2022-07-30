45cf116c11dd9c27b8ce657ca7b1b1fe1d43475e
Akshay Kumar <93372936+Akshay-Kumar2000@users.noreply.github.com>
2022-07-30 12:37:08 +0530

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,40 @@
+import "./home.css";
+import { io } from "socket.io-client";
+import Chat from "../../components/Chat/Chat";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { useState } from "react";
+
+const socket = io.connect("https://quiet-cove-46644.herokuapp.com/");
+
+const Home = () => {
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  return (
+    <div className="main">
+      <Sidebar
+        room={room}
+        socket={socket}
+        setRoom={setRoom}
+        userName={userName}
+        showChat={showChat}
+        setUserName={setUserName}
+        setShowChat={setShowChat}
+      />
+      {showChat ? (
+        <Chat
+          room={room}
+          socket={socket}
+          setRoom={setRoom}
+          userName={userName}
+          setUserName={setUserName}
+        />
+      ) : (
+        <div className="chat"></div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
